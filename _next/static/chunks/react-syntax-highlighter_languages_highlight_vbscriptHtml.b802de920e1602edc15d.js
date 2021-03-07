81c1f77fbbb68cf1c71f558813449cd22bb6f215
fullalien <baifanf@hotmail.com>
2021-03-07 12:57:22 +0000

    Deploying to gh-pages from  @ 5bd7ee0e1c2612c63511ccf44a83e26aef10372a 🚀

---
+++ _next/static/chunks/react-syntax-highlighter_languages_highlight_vbscriptHtml.b802de920e1602edc15d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[194],{"9Fqr":function(n,e){n.exports=function(n){return{name:"VBScript in HTML",subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
