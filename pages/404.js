60fe16a5ba4276c92417d781628abc20d3b98b26
van <baifanf@hotmail.com>
2021-03-14 16:47:33 +0800

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import CustomHead from "../components/custom-head";
 import styles from '../styles/404.module.css'
 
 export default function ErrorPage() {
     return (
         <div className={styles.container}>
+            <CustomHead title="404"/>
             <div className={styles.content}>
                 <span className={styles.error_code}>404</span>
                 <span className={styles.error_info}>This page could not be found.</span>
