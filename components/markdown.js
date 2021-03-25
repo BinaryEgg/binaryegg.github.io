05f8139602cc46fdb1af388657e34a6daf745068
van <baifanf@hotmail.com>
2021-03-25 22:36:30 +0800

    custom markdown

--- components/markdown.js
+++ components/markdown.js
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types'
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown/with-html'
+import Gfm from 'remark-gfm'
 import CodeBlock from './code-block'
 
 export default function Markdown({ source }) {
     return (
         <ReactMarkdown
-            source={source}
+            children={source}
             renderers={{
                 code: CodeBlock,
             }}
+            plugins={[Gfm]}
+            allowDangerousHtml={true}
+            escapeHtml={false}
         />
     );
 }
