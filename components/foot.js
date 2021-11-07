8ca58ee64172f4abb99f36f7073a7759c6ffc279
van <baifanf@hotmail.com>
2021-11-07 10:42:47 +0800

    add twitter

--- components/foot.js
+++ components/foot.js
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 const Foot = () => (
     <div className={styles.container}>
-        <p><a href="mailto:contact@binaryegg.com">Email</a>﹒<a href='/feed.xml'>RSS</a></p>
+        <p><a href="https://twitter.com/binary_egg">Twitter</a>﹒<a href="mailto:contact@binaryegg.com">Email</a>﹒<a href='/feed.xml'>RSS</a></p>
         <p><Link href='/privacy-policy'><a>Privacy Policy</a></Link></p>
         <div className={styles.rights_info}><p>© 2021 BinaryEgg. All Rights Reserved.</p></div>
     </div>
