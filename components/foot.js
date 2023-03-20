afcdd5a93e23d5738942389f67b2d978a56e2552
van <baifanf@hotmail.com>
2023-03-20 21:11:58 +0800

    update copyright

--- components/foot.js
+++ components/foot.js
@@ -5,7 +5,7 @@ const Foot = () => (
     <div className={styles.container}>
         <p><a href="https://twitter.com/binary_egg">Twitter</a>﹒<a href="mailto:contact@binaryegg.com">Email</a>﹒<a href='/feed.xml'>RSS</a></p>
         <p><Link href='/privacy-policy'><a>Privacy Policy</a></Link></p>
-        <div className={styles.rights_info}><p>© 2021 BinaryEgg. All Rights Reserved.</p></div>
+        <div className={styles.rights_info}><p>© 2023 BinaryEgg. All Rights Reserved.</p></div>
     </div>
 )
 
